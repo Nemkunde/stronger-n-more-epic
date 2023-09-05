24555d087e709ca4cd9eb3bee6c6ea6d145371ac
Erica Eklund <erica@iMac-som-tillhor-Erica.local>
2023-09-05 13:49:46 +0200

    structure added to project

---
+++ client/src/components/abstracts/PopUpComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PopUpComponent = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PopUpComponent
---
+++ client/src/pages/UserBookingPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserBookingPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UserBookingPage
