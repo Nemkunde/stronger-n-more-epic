3f2b9fc2c834c628c9a84a6174bd47880efd619d
Erica Eklund <erica@iMac-som-tillhor-Erica.local>
2023-09-06 17:59:43 +0200

    fixed provisional db.js

--- client/src/components/abstracts/GridCard.tsx
+++ client/src/components/abstracts/GridCard.tsx
@@ -1,78 +1,5 @@
 import React from 'react'
-const passes = [
-    {
-      pass: "Calming Yoga",
-      day: "monday",
-      date: "20230819",
-      time: "9.30 AM",
-      coach: "Strongy McStrong",
-      description: "Random text about the exercise, location and such.",
-    },
-    {
-      pass: "Group Training",
-      day: "sunday",
-      date: "20230819",
-      time: "10.50 AM",
-      coach: "Strongy McStrong",
-      description: "Random text about the exercise, location and such.",
-    },
-    {
-      pass: "Cardio",
-      day: "monday",
-      date: "20230819",
-      time: "1.30 PM",
-      coach: "Strongy McStrong",
-      description: "Random text about the exercise, location and such.",
-    },
-    {
-      pass: "Advanced Yoga",
-      day: "monday",
-      date: "20230819",
-      time: "11 AM",
-      coach: "Strongy McStrong",
-      description: "Random text about the exercise, location and such.",
-    },
-    {
-      pass: "Boxing",
-      day: "wednesday",
-      date: "20230819",
-      time: "5.15 PM",
-      coach: "Strongy McStrong",
-      description: "Random text about the exercise, location and such.",
-    },
-    {
-        pass: "Boxing",
-        day: "saturday",
-        date: "20230819",
-        time: "5.15 PM",
-        coach: "Strongy McStrong",
-        description: "Random text about the exercise, location and such.",
-      },
-      {
-        pass: "Yoga",
-        day: "saturday",
-        date: "20230819",
-        time: "5.15 PM",
-        coach: "Strongy McStrong",
-        description: "Random text about the exercise, location and such.",
-      },
-      {
-        pass: "Swimming",
-        day: "saturday",
-        date: "20230819",
-        time: "5.15 PM",
-        coach: "Strongy McStrong",
-        description: "Random text about the exercise, location and such.",
-      },
-      {
-        pass: "Boxing",
-        day: "friday",
-        date: "20230819",
-        time: "5.15 PM",
-        coach: "Strongy McStrong",
-        description: "Random text about the exercise, location and such.",
-      },
-  ];
+import exportPasses from './db';
 
 type DayProps = {
     dayText: string, 
@@ -83,7 +10,7 @@ const GridCard = ({dayText, dayString}: DayProps) => {
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: "5px" }}>
           <p style={{ textAlign: "center" }}>{dayText}</p>
-          {passes.map((pass) =>
+          {exportPasses().map((pass) =>
             pass.day === dayString ? (
               <div
                 key={pass.day}
