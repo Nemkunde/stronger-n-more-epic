6a15c543e5a5c00b403d04d46665a0824878d759
Erica Eklund <erica@iMac-som-tillhor-Erica.local>
2023-09-06 17:30:34 +0200

    provisional grid for gym sessions

--- client/src/components/CalenderComponent.tsx
+++ client/src/components/CalenderComponent.tsx
@@ -3,6 +3,7 @@ import React from "react";
 const passes = [
   {
     pass: "Calming Yoga",
+    day: "monday",
     date: "20230819",
     time: "9.30 AM",
     coach: "Strongy McStrong",
@@ -10,6 +11,7 @@ const passes = [
   },
   {
     pass: "Group Training",
+    day: "sunday",
     date: "20230819",
     time: "10.50 AM",
     coach: "Strongy McStrong",
@@ -17,6 +19,7 @@ const passes = [
   },
   {
     pass: "Cardio",
+    day: "monday",
     date: "20230819",
     time: "1.30 PM",
     coach: "Strongy McStrong",
@@ -24,6 +27,7 @@ const passes = [
   },
   {
     pass: "Advanced Yoga",
+    day: "monday",
     date: "20230819",
     time: "11 AM",
     coach: "Strongy McStrong",
@@ -31,6 +35,7 @@ const passes = [
   },
   {
     pass: "Boxing",
+    day: "wednesday",
     date: "20230819",
     time: "5.15 PM",
     coach: "Strongy McStrong",
@@ -39,16 +44,166 @@ const passes = [
 ];
 
 const CalenderComponent = () => {
-  return <div style={{ position: "absolute",  left: "2em", right: "2em", display:"flex", flexDirection: "column", gap: "5px"}}>
-    {passes.map(pass => (
-        <div style={{width: "150px", background: "orange", padding: "5px", textAlign: "center", borderRadius: "7px"}}>
-        <p key={pass.pass}>{pass.pass}</p>
-        <p>{pass.date}</p>
-        <p>{pass.time}</p>
+  return (
+    <div>
+      <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+        <div style={{ display: "flex", flexDirection: "column", gap: "5px" }}>
+          <p style={{ textAlign: "center" }}>Mon</p>
+          {passes.map((pass) =>
+            pass.day === "monday" ? (
+              <div
+                key={pass.day}
+                style={{
+                  width: "100%",
+                  background: "orange",
+                  padding: "5px",
+                  textAlign: "center",
+                  borderRadius: "7px",
+                }}>
+                <p>{pass.pass}</p>
+                <p>{pass.date}</p>
+                <p>{pass.time}</p>
+              </div>
+            ) : (
+              ""
+            )
+          )}
         </div>
-        
-    ))}
-  </div>;
+        <div style={{ display: "flex", flexDirection: "column", gap: "5px" }}>
+          <p style={{ textAlign: "center" }}>Tues</p>
+          {passes.map((pass) =>
+            pass.day === "tuesday" ? (
+              <div
+                key={pass.day}
+                style={{
+                  width: "100%",
+                  background: "orange",
+                  padding: "5px",
+                  textAlign: "center",
+                  borderRadius: "7px",
+                }}>
+                <p>{pass.pass}</p>
+                <p>{pass.date}</p>
+                <p>{pass.time}</p>
+              </div>
+            ) : (
+              ""
+            )
+          )}
+        </div>
+        <div style={{ display: "flex", flexDirection: "column", gap: "5px" }}>
+          <p style={{ textAlign: "center" }}>Wed</p>
+          {passes.map((pass) =>
+            pass.day === "wednesday" ? (
+              <div
+                key={pass.day}
+                style={{
+                  width: "100%",
+                  background: "orange",
+                  padding: "5px",
+                  textAlign: "center",
+                  borderRadius: "7px",
+                }}>
+                <p>{pass.pass}</p>
+                <p>{pass.date}</p>
+                <p>{pass.time}</p>
+              </div>
+            ) : (
+              ""
+            )
+          )}
+        </div>
+        <div style={{ display: "flex", flexDirection: "column", gap: "5px" }}>
+          <p style={{ textAlign: "center" }}>Thur</p>
+          {passes.map((pass) =>
+            pass.day === "thursday" ? (
+              <div
+                key={pass.day}
+                style={{
+                  width: "100%",
+                  background: "orange",
+                  padding: "5px",
+                  textAlign: "center",
+                  borderRadius: "7px",
+                }}>
+                <p>{pass.pass}</p>
+                <p>{pass.date}</p>
+                <p>{pass.time}</p>
+              </div>
+            ) : (
+              ""
+            )
+          )}
+        </div>
+        <div style={{ display: "flex", flexDirection: "column", gap: "5px" }}>
+          <p style={{ textAlign: "center" }}>Fri</p>
+          {passes.map((pass) =>
+            pass.day === "friday" ? (
+              <div
+                key={pass.day}
+                style={{
+                  width: "100%",
+                  background: "orange",
+                  padding: "5px",
+                  textAlign: "center",
+                  borderRadius: "7px",
+                }}>
+                <p>{pass.pass}</p>
+                <p>{pass.date}</p>
+                <p>{pass.time}</p>
+              </div>
+            ) : (
+              ""
+            )
+          )}
+        </div>
+        <div style={{ display: "flex", flexDirection: "column", gap: "5px" }}>
+          <p style={{ textAlign: "center" }}>Sat</p>
+          {passes.map((pass) =>
+            pass.day === "saturday" ? (
+              <div
+                key={pass.day}
+                style={{
+                  width: "100%",
+                  background: "orange",
+                  padding: "5px",
+                  textAlign: "center",
+                  borderRadius: "7px",
+                }}>
+                <p>{pass.pass}</p>
+                <p>{pass.date}</p>
+                <p>{pass.time}</p>
+              </div>
+            ) : (
+              ""
+            )
+          )}
+        </div>
+        <div style={{ display: "flex", flexDirection: "column", gap: "5px" }}>
+          <p style={{ textAlign: "center" }}>Sun</p>
+          {passes.map((pass) =>
+            pass.day === "sunday" ? (
+              <div
+                key={pass.day}
+                style={{
+                  width: "100%",
+                  background: "orange",
+                  padding: "5px",
+                  textAlign: "center",
+                  borderRadius: "7px",
+                }}>
+                <p>{pass.pass}</p>
+                <p>{pass.date}</p>
+                <p>{pass.time}</p>
+              </div>
+            ) : (
+              ""
+            )
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CalenderComponent;
