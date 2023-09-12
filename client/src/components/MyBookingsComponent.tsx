1a2d9080bd678dec8d9d44a78dea76c6682e3b97
Erica Eklund <erica@iMac-som-tillhor-Erica.local>
2023-09-12 12:10:06 +0200

    had an issue with useEffect so removed the problem by calling the function when showing the popup instead

--- client/src/components/MyBookingsComponent.tsx
+++ client/src/components/MyBookingsComponent.tsx
@@ -1,57 +1,24 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
-import { User} from '../types/Users'
+import React from "react";
 
+type ActivityProps = {
+    setActivites: string[]
+}
 
-const MyBookingsComponent = () => {
-    const [activities, setActivities] = useState<User[]>([]);
-    const [newArray, setNewArray] = useState<string[]>([]);
-
-  useEffect(() => {
-    fetch("/api/admin/users")
-      .then((res) => res.json())
-      .then((json) => setActivities(json.users))
-      .catch((err) => console.log(err));
-
-      const key = "username"; 
-      const user = localStorage.getItem(key);
-
-      activities.map((activity) => {
-        if(user === activity.username) {
-            console.log(activity.activities);
-            if (activity.activities !== undefined){
-            const session = activity.activities;
-            setNewArray(session);
-            console.log(newArray);
-            }
-            // setNewArray(session);
-        }
-      })
-     
-  }, []);
-
+const MyBookingsComponent = ({setActivites}: ActivityProps) => {
   return (
     <div className="modal" style={{ display: "block" }}>
       <div className="modal-dialog">
         <div className="modal-content">
           <div className="modal-body">
             <p>My bookings</p>
-            {newArray?.map((activities) => (
-                <div key={activities}>{activities}</div>
+            {setActivites?.map((activities) => (
+              <div key={activities}>{activities}</div>
             ))}
           </div>
         </div>
       </div>
     </div>
-    // <div>
-    // <p>My bookings</p>
-    //   {users.map((user) => (
-    //     <div key={user.id}>
-            
-    //         {user.activities}</div>
-    //   ))}
-    // </div>
-  )
-}
+  );
+};
 
-export default MyBookingsComponent
+export default MyBookingsComponent;
--- client/src/pages/UserBookingPage.tsx
+++ client/src/pages/UserBookingPage.tsx
@@ -4,13 +4,45 @@ import Footer from "../components/Footer";
 import CalenderComponent from "../components/CalenderComponent";
 import { useState } from "react";
 import MyBookingsComponent from "../components/MyBookingsComponent";
+import { User } from '../types/Users';
 
 const UserBookingPage = () => {
   const [showBookings, setShowBookings] = useState(true);
+  const [activities, setActivities] = useState<User[]>([]);
+  const [newArray, setNewArray] = useState<string[]>([]);
 
   const handleBookingClick: React.MouseEventHandler<HTMLDivElement> = (e) => {
     setShowBookings(false);
+    getBookings();
   };
+
+  const closeWindow: React.MouseEventHandler<HTMLDivElement> = (e) => {
+    setShowBookings(true);
+  }
+
+  const getBookings = () => {
+    fetch("/api/admin/users")
+      .then((res) => res.json())
+      .then((json) => setActivities(json.users))
+      .catch((err) => console.log(err));
+
+    const key = "username";
+    const user = localStorage.getItem(key);
+
+    activities.map((activity) => {
+      if (user === activity.username) {
+        console.log(activity.activities);
+        if (activity.activities !== undefined) {
+          const session = activity.activities;
+          setNewArray(session);
+          console.log(newArray);
+        }
+        // setNewArray(session);
+      }
+    });
+  }
+  
+
   return (
     <div>
       <Header btnText={"Logout"} />
@@ -18,7 +50,9 @@ const UserBookingPage = () => {
         <div className="my-3 btn btn-primary me-2" onClick={handleBookingClick}>
           My bookings
         </div>
-        {showBookings === false && <MyBookingsComponent />}
+        <div onClick={closeWindow}>
+        {showBookings === false && <MyBookingsComponent setActivites={newArray}/> }
+        </div>
       </div>
       <CalenderComponent />
       <Footer />
