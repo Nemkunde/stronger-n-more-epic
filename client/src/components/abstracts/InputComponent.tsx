5266b4a0cb768b269ee3fe94339c851573ac2203
Nemkunde <nemkunde.liljemark@student.kyh.se>
2023-09-14 17:06:56 +0200

    ts update

--- client/src/components/abstracts/InputComponent.tsx
+++ client/src/components/abstracts/InputComponent.tsx
@@ -1,4 +1,45 @@
-import React from 'react'
+// @ts-check
+
+import React, { ChangeEvent } from 'react';
+
+type InputProps = {
+  placeholderText: string;
+  activityValue: string;
+  handleInputChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  name: string;
+  label: string;
+  required?: boolean;
+  autofocus?: boolean;
+};
+
+const InputComponent: React.FC<InputProps> = ({
+  label,
+  handleInputChange,
+  activityValue,
+  placeholderText,
+  name,
+  required,
+  autofocus,
+}) => {
+  return (
+    <div className="mb-3">
+      <label className="form-label">{label}</label>
+      <input
+        type="text"
+        className="form-control"
+        name={name}
+        placeholder={placeholderText}
+        value={activityValue}
+        onChange={handleInputChange}
+        required={required}
+        autoFocus={autofocus}
+      />
+    </div>
+  );
+};
+
+export default InputComponent;
+/*import React from 'react'
 
 type InputProps = {
     placeholderText: string,
@@ -28,4 +69,4 @@ const InputComponent: React.FC<InputProps> = ({ label, handleInputChange, activi
   )
 }
 
-export default InputComponent
+export default InputComponent*/
